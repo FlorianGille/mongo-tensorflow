2093a01a7c8d6e4ff7a15610946f747cff9a506f
Paul Godard <paul.godard@outlook.fr>
2020-12-09 19:29:34 +0100

    implement tensorflow-models

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+require('@tensorflow/tfjs-backend-cpu');
+require('@tensorflow/tfjs-backend-webgl');
+const cocoSsd = require('@tensorflow-models/coco-ssd');
+// const mobilenet = require('@tensorflow-models/mobilenet');
 
-function App() {
+const logo = require('./assets/moi.png');
+
+const App = () => {
+
+  const detectImg = async() => {
+    const img = document.getElementById('img');
+    // Load the model.
+    const model = await cocoSsd.load();
+    // const model = await mobilenet.load();
+    // Classify the image.
+    const predictions = await model.detect(img);
+    // const predictions = await model.classify(img);
+    console.log('Predictions: ');
+    console.log(predictions);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img id="img" src={logo.default} alt="logo" crossOrigin='anonymous'/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={detectImg}> Detect </button>
         <a
           className="App-link"
           href="https://reactjs.org"
